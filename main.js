9f8cb4744ad620cd4451ddaf18e70ba512880c03
Joaquin Ogallar <joaquinnogallar@gmail.com>
2024-12-12 14:57:29 -0300

    feat: add skills and its containers styles

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-const url = "./assets/data.json"
-const d = document
+const url = "./assets/data.json";
+const d = document;
 
 /*
-JSON STRUCTURE
+JSON PROJECT STRUCTURE
 {
     "id": 0,
     "image": "",
@@ -12,104 +12,103 @@ JSON STRUCTURE
     "github": "https://github.com/"
 } */
 
-const $meExperiencesContainer = d.querySelector(".me-experiences-container")
+const $meExperiencesContainer = d.querySelector(".me-experiences-container");
 
 const getProjects = async () => {
-    let data = await fetch(url)
-    let json = await data.json()
+  let data = await fetch(url);
+  let json = await data.json();
 
-    if(json.length === 0) {
-        let empty = d.createElement("p")
-        empty.id = "no-projects"
-        empty.textContent = "No projects available..."
-        $meExperiencesContainer.appendChild(empty)
-    } else {
-        json.forEach(proj => {
-            // main container of the project, it contains all the information
-            let projContainer = d.createElement("div")
-            projContainer.setAttribute("id", proj.id)
-            projContainer.classList.add("me-e-container")
-    
-            // image of the project
-            let projImg = d.createElement("img")
-            projImg.classList.add("me-e-c-img")
-            projImg.src = proj.image
-            
-            projContainer.appendChild(projImg)
-    
-            // project text and description
-            let textContainer = d.createElement("div")
-            textContainer.classList.add("me-e-i-text")
-    
-            let projTitle = d.createElement("h3")
-            projTitle.textContent = proj.title
-            projTitle.classList.add("project-title")
-    
-            let projDescription = d.createElement("p")
-            projDescription.classList.add("roboto-mono")
-            projDescription.textContent = proj.description
-    
-            textContainer.appendChild(projTitle)
-            textContainer.appendChild(projDescription)
-    
-            // project technologies
-            let techContainer = d.createElement("div")
-            techContainer.classList.add("me-e-i-techs")
-    
-            let projTechsTitle = d.createElement("h3")
-            projTechsTitle.classList.add("project-title")
-            projTechsTitle.textContent = "Technologies"
-    
-            techContainer.appendChild(projTechsTitle)
-            
-            let techsContainer = d.createElement("div")
-            techsContainer.classList.add("me-e-t-images")
-            proj.techs.forEach(t => {
-                let tImg = d.createElement("img")
-                tImg.src = `./assets/img/techs/${t}.svg`
-                techsContainer.appendChild(tImg)            
-            })
-            
-            techContainer.appendChild(techsContainer)
-    
-            // buttons github and more
-            let linksContainer = d.createElement("div");
-            linksContainer.classList.add("me-e-i-links")
+  if (json.length === 0) {
+    let empty = d.createElement("p");
+    empty.id = "no-projects";
+    empty.textContent = "No projects available...";
+    $meExperiencesContainer.appendChild(empty);
+  } else {
+    json.forEach((proj) => {
+      // main container of the project, it contains all the information
+      let projContainer = d.createElement("div");
+      projContainer.setAttribute("id", proj.id);
+      projContainer.classList.add("me-e-container");
+      if (proj.id % 2 !== 0) projContainer.classList.add("odd-project");
 
-            let ghImg = d.createElement("img")
-            ghImg.src = "./assets/img/techs/github.svg"
-    
-            let ghLink = d.createElement("a")
-            ghLink.id = "gh-link"
-            ghLink.href = proj.github
-            ghLink.target ="_blank"
-            
-            ghLink.appendChild(ghImg)
-            ghLink.append(" GitHub")
-    
-            let moreLink = d.createElement("a")
-            moreLink.id = "more-link"
-            moreLink.textContent = "More..."
-            moreLink.href = `./project.html/${proj.id}`
-    
-            linksContainer.appendChild(ghLink)
-            linksContainer.appendChild(moreLink)
-    
-            // text, description and technologies container
-            let infoContainer = d.createElement("div")
-            infoContainer.classList.add("me-e-information")
-    
-            infoContainer.appendChild(textContainer)
-            infoContainer.appendChild(techContainer)
-            infoContainer.appendChild(linksContainer)
-    
-            projContainer.appendChild(infoContainer)
-    
-            $meExperiencesContainer.appendChild(projContainer)
-    
-        })
-    }
-    
-}
+      // image of the project
+      let projImg = d.createElement("img");
+      projImg.classList.add("me-e-c-img");
+      projImg.src = proj.image;
 
-getProjects()
+      projContainer.appendChild(projImg);
+
+      // project text and description
+      let textContainer = d.createElement("div");
+      textContainer.classList.add("me-e-i-text");
+
+      let projTitle = d.createElement("h3");
+      projTitle.textContent = proj.title;
+      projTitle.classList.add("project-title");
+
+      let projDescription = d.createElement("p");
+      projDescription.classList.add("roboto-mono");
+      projDescription.textContent = proj.description;
+
+      textContainer.appendChild(projTitle);
+      textContainer.appendChild(projDescription);
+
+      // project technologies
+      let techContainer = d.createElement("div");
+      techContainer.classList.add("me-e-i-techs");
+
+      let projTechsTitle = d.createElement("h3");
+      projTechsTitle.classList.add("project-title");
+      projTechsTitle.textContent = "Technologies";
+
+      techContainer.appendChild(projTechsTitle);
+
+      let techsContainer = d.createElement("div");
+      techsContainer.classList.add("me-e-t-images");
+      proj.techs.forEach((t) => {
+        let tImg = d.createElement("img");
+        tImg.src = `./assets/img/techs/${t}.svg`;
+        techsContainer.appendChild(tImg);
+      });
+
+      techContainer.appendChild(techsContainer);
+
+      // buttons github and more
+      let linksContainer = d.createElement("div");
+      linksContainer.classList.add("me-e-i-links");
+
+      let ghImg = d.createElement("img");
+      ghImg.src = "./assets/img/techs/github.svg";
+
+      let ghLink = d.createElement("a");
+      ghLink.id = "gh-link";
+      ghLink.href = proj.github;
+      ghLink.target = "_blank";
+
+      ghLink.appendChild(ghImg);
+      ghLink.append(" GitHub");
+
+      let moreLink = d.createElement("a");
+      moreLink.id = "more-link";
+      moreLink.textContent = "More...";
+      moreLink.href = `./project.html/${proj.id}`;
+
+      linksContainer.appendChild(ghLink);
+      linksContainer.appendChild(moreLink);
+
+      // text, description and technologies container
+      let infoContainer = d.createElement("div");
+      infoContainer.classList.add("me-e-information");
+
+      infoContainer.appendChild(textContainer);
+      infoContainer.appendChild(techContainer);
+      infoContainer.appendChild(linksContainer);
+
+      projContainer.appendChild(infoContainer);
+
+      $meExperiencesContainer.appendChild(projContainer);
+    });
+  }
+};
+
+getProjects();
