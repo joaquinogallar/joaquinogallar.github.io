63331032aa9cca6e37bc638480ca2d1b9b71cb03
Joaco <joaquinnogallar@gmail.com>
2024-12-29 18:34:06 -0300

    feat: return back to home functionality in projects, and information in more...

--- project.js
+++ project.js
@@ -3,12 +3,19 @@ import ProjectService from "./main.js";
 const urlParams = new URLSearchParams(window.location.search);
 
 const id = urlParams.get("id");
-console.log(id);
 
 const title = document.getElementById("project-title");
+const pTitle = document.getElementById("p-title");
+
+const pImg = document.getElementById("p-img");
+
+const pDescription = document.getElementById("p-description");
 
 (async () => {
   const proj = await ProjectService.findById(parseInt(id)); // id came as a a string, so I need to parse it to an integer for the method to work
 
   title.textContent += ` - ${proj.title}`;
+  pTitle.textContent = proj.title
+  
+  pDescription.textContent = proj.description
 })();
