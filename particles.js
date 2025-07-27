30f3fef8ee8a9f5522f3d03ab398647567bf442b
Joaquin Ogallar <joaquinnogallar@gmail.com>
2025-07-27 13:21:05 -0300

    feat: added particle generator effect

---
+++ particles.js
@@ -0,0 +1,22 @@
+const container = document.getElementById('particle-container');
+const bubbleCount = 50;
+const minSpeed = 10000;   // in ms
+const maxSpeed = 20000;
+const maxDelay = 10000;
+
+for (let i = 0; i < bubbleCount; i++) {
+    const bubble = document.createElement('div');
+    bubble.classList.add('bubble');
+
+    // random position
+    bubble.style.left = Math.random() * 100 + '%';
+
+    // random animation duration and delay
+    const duration = Math.floor(Math.random() * (maxSpeed - minSpeed)) + minSpeed;
+    const delay = Math.floor(Math.random() * maxDelay);
+
+    bubble.style.animationDuration = `${duration}ms`;
+    bubble.style.animationDelay = `${delay}ms`;
+
+    container.appendChild(bubble);
+}
